a8644c2cecedd31cf3add40ec85914fbb8e708c9
Weslley Rodrigo <wrodrigo@ibm.com>
2023-01-16 21:20:37 -0300

    Respond 'ping' with RESP of 'pong'

--- app/server.go
+++ app/server.go
@@ -18,11 +18,20 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
 	fmt.Println("Server listening to port 6379")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go func(conn net.Conn) {
+			conn.Write([]byte("+PONG\r\n"))
+			conn.Close()
+		}(conn)
+
+	}
 }
